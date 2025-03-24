cb71c62af4786c846af033c571121bf460b8497a
kpto09 <20728761+kpto09@users.noreply.github.com>
2025-03-23 21:07:24 -0700

    added error handling

--- src/index.ts
+++ src/index.ts
@@ -7,51 +7,65 @@ dotenv.config();
 
 const readFile = util.promisify(fs.readFile);
 
+async function atpLogin(): Promise<AtpAgent> {
+  try {
+    const agent = new AtpAgent({ service: 'https://bsky.social' })
+
+    await agent.login({
+      identifier: process.env.BSKY_IDENTIFIER,
+      password: process.env.BSKY_PASSWORD
+    });
+
+    return agent;
+  } catch (error) {
+      console.log('Error while logging into BlueSky: ', error);
+  }
+}
+
+// Converts .jpg image buffer to a Uint8Array
 async function loadImageData(path: fs.PathLike): Promise<{imageData: Uint8Array}> {
-  // Read the file from the provided path
   const buffer = await readFile(path);
 
-  // Convert the buffer to a Uint8Array and return it
   return { imageData: new Uint8Array(buffer.buffer, buffer.byteOffset, buffer.byteLength) };
 }
 
-async function atpLogin(): Promise<AtpAgent> {
-  const agent = new AtpAgent({ service: 'https://bsky.social' })
-
-  await agent.login({
-    identifier: process.env.BSKY_IDENTIFIER,
-    password: process.env.BSKY_PASSWORD
-  });
-
-  return agent
+// Uploads the image to BlueSky servers
+async function uploadImage(imageData: Uint8Array ,agent: AtpAgent) {
+  try {
+    const uploadedImage = await agent.uploadBlob(imageData, { encoding: 'image/jpg' });
+    return uploadedImage;
+  } catch (error) {
+    console.log('Error while uploading image to BlueSky: ', error);
+  }
 }
 
-async function postImage(agent: AtpAgent): Promise<void> {
-
-  // Converts the image from path to Uint8Array
-  const { imageData } = await loadImageData('./images/mrkrabs_day15.jpg');
-
-  const uploadImage = await agent.uploadBlob(imageData, { encoding: 'image/jpg' });
-
-  await agent.post({
-    text: "GIVE IT UP FOR DAY 15",
-    embed: {
-      images: [
-        {
-          image: uploadImage.data.blob,
-          alt: 'Mr. Krabs ringing the bell to signal it is day 15 of the month.'
-        },
-      ],
-      $type: 'app.bsky.embed.images',
-    },
-  });
+// Make post to BlueSky
+async function postImage(uploadedImage, agent: AtpAgent): Promise<void> {
+  try {
+    await agent.post({
+      text: "GIVE IT UP FOR DAY 15",
+      embed: {
+        images: [
+          {
+            image: uploadedImage.data.blob,
+            alt: 'Mr. Krabs ringing the bell to signal it is day 15 of the month.'
+          },
+        ],
+        $type: 'app.bsky.embed.images',
+      },
+    });
+  } catch (error) {
+    console.log("Error posting to BlueSky: ", error);
+  }
 }
 
 async function main() {
   const agent = await atpLogin();
-  await postImage(agent);
+  const { imageData } = await loadImageData('./images/mrkrabs_day15.jpg');
+  const uploadedImage = await uploadImage(imageData, agent);
+  await postImage(uploadedImage, agent);
 }
 
 main()
 
-// export default postImage
+export default {atpLogin, uploadImage}
